77f60f802898e440f16a674e7073cfeba2400cd2
unknown <tecmatheusg@hotmail.com>
2023-07-01 18:01:49 -0300

    Títulos centralizados em telas menores

--- src/components/navbarComponent/index.js
+++ src/components/navbarComponent/index.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom border-white">
             <div className="container-fluid">
                 <img id="img-logo" className="ms-lg-4" src={logoNavbar} />
-                <Link className="navbar-brand ps-3 " to="/">Projeto Ressignificar</Link>
+                <Link className="navbar-brand ps-lg-3" to="/">Projeto Ressignificar</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>

4ec9ecf50f9f06a544e3967ffd54718356757748
unknown <tecmatheusg@hotmail.com>
2023-11-28 23:28:47 -0300

    Ajuste alinhamento titulo

--- src/components/titleComponent/index.js
+++ src/components/titleComponent/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Title({ title }) {
   return (
     <>
-    <div className="ms-2 d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block px-2 ps-xxl-5 py-3">
+    <div className="ms-md-2 d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block px-2 ps-xxl-5 py-3">
       <h2 className="display-2 fw-medium m-2 about-heading">
         {title}
       </h2>
     </div>
     
-    <div className="ms-2 d-block d-sm-none px-2 ps-xxl-5 py-3">
+    <div className="ms-md-2 d-block d-sm-none px-2 ps-xxl-5 py-3">
       <h2 className="display-2 text-center fw-medium mt-2 mb-0 mb-lg-5 mb-xl-5 mb-xxl-5 about-heading">
         {title}
       </h2>

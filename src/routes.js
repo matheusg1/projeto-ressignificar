b8bf1b177882d3c1248e6ecef8eebd652e370ba0
unknown <tecmatheusg@hotmail.com>
2024-04-27 21:23:57 -0300

    reorganização de pastas e arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 import { React, } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import About from './pages/about';
 import Feedback from './pages/dar-feedback';
-import Index from './pages/index';
 import AcompanhamentoPsicologicoForm from './pages/acompanhamento-psicologico';
 import AcompanhamentoPsiquiatricoForm from './pages/acompanhamento-psiquiatrico';
 import AcompanhamentoNutricionalForm from './pages/acompanhamento-nutricional';
 import AtendimentoPresencial from './pages/locais';
 import Posts from './pages/posts';
-import Inicio from './pages/about2';
+import Inicio from './pages/index';
 
 export default function AppRoutes() {
 
@@ -16,7 +14,7 @@ export default function AppRoutes() {
         <Routes>
             <Route path="*" element={<Navigate to="/" replace />} />
             <Route path="/" element={<Inicio />} />
-            <Route path="/sobre" element={<About />} />
+            {/* <Route path="/sobre" element={<About />} /> */}
             {/* <Route path="/sobre2" element={<About2 />} /> */}
             <Route path="/posts" element={<Posts />} />
             <Route path="/atendimento-presencial" element={<AtendimentoPresencial />} />
